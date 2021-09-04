f5387b315ab41204d8c1a27bf9fb02a946f08c2e
yeonjiyeon <dalnim95@naver.com>
2021-09-04 09:52:30 +0900

    commit

--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,9 +2,12 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
 
-    private MemberRepository memberRepository = new MemoryMemberRepository();
+    //private MemberRepository memberRepository = new MemoryMemberRepository();--> AppConfig에 등록해서 구현체를 지워준다
     //null값이 들어오지 않게 구현체를 생성해 준다
-
+    private final MemberRepository memberRepository;
+    public MemberServiceImpl(MemberRepository memberRepository){//AppConfig에서 생성자를 호출하면서 구현체를 넣어준다
+        this.memberRepository = memberRepository;
+    }
     @Override
     public void join(Member member) {
         memberRepository.save(member);
--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -5,17 +5,23 @@ import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
     //주문 생성 요청이 오면, 회원 정보를 조회하고, 할인 정책을 적용한 다음 주문 객체를 생성해서 반환한다.
     //메모리 회원 리포지토리와, 고정 금액 할인 정책을 구현체로 생성한다.
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
     //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
     //private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
     private DiscountPolicy discountPolicy;//인터페이스에만 의존
 
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
